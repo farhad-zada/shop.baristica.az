eb4bac6d6ced221341f9954a10b6164cdea50027
farhad-zada <farhad-zada@users.noreply.github.com>
2024-07-26 04:34:21 +0000

    deploy: 2098f762f1077d28b98f045ea54a98ca00551aa4

--- src/config/index.js
+++
@@ -1,2 +0,0 @@
-const HOST_URL = process.env.REACT_APP_BASE_URL
-export { HOST_URL }
--- src/redux/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import baristicaReducer from './slice/index'
-
-
-export const store = configureStore({
-    reducer : {
-        baristica : baristicaReducer
-    }
-})
